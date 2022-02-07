269775b75ec130c691be0a40e9eb925864393c3d
Corné Veldman <cveldman@hotmail.com>
2022-02-07 15:12:40 +0100

    language

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -4,7 +4,7 @@
             <tr>
                 @foreach($columns as $column)
                     <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                        {{ $column }}
+                        {{ __($column) }}
                     </th>
                 @endforeach
             </tr>

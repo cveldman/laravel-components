272ca0900759fcddaaf36268a481dc3186b35a3f
Corné Veldman <cveldman@hotmail.com>
2022-02-07 15:01:46 +0100

    table

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
     <x-label :for="$name" :label="$label"></x-label>
 
-    <select name="{{ $name }}"  class="form-control @error($name) is-invalid @enderror" id="{{ $name }}" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+    <select name="{{ $name }}" id="{{ $name }}" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
         @foreach($options as $k => $v)
             <option value="{{ $k }}" @if($k == $value) selected @endif>{{ $v }}</option>
         @endforeach
--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -2,9 +2,7 @@
     <x-label :for="$name" :label="$label"></x-label>
 
     <div class="mt-1">
-        <textarea name="{{ $name }}" id="{{ $name }}" class="form-control @error($name) is-invalid @enderror" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
-            {{ $value }}
-        </textarea>
+        <textarea name="{{ $name }}" id="{{ $name }}" rows="3" class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">{{ $value }}</textarea>
     </div>
 
     <x-error :name="$name"></x-error>

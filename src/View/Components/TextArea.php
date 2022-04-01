16c4f11ee6475b109bff9b1e60a4a0fca6abb5d4
Corné Veldman <cveldman@hotmail.com>
2022-04-01 13:34:18 +0200

    Update TextArea.php

--- src/View/Components/TextArea.php
+++ src/View/Components/TextArea.php
@@ -6,14 +6,12 @@ use Illuminate\View\Component;
 
 class TextArea extends Component
 {
-    public $type;
     public $name;
     public $value;
     public $label;
 
-    public function __construct($type, $name, $value = null, $label = null)
+    public function __construct($name, $value = null, $label = null)
     {
-        $this->type = $type;
         $this->name = $name;
         $this->value = old($name, $value);
         $this->label = ucfirst(str_replace("_", " ", $name));
